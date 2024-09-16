49c01bde589df860e00269e56828ba1f78151ddf
Andres Javier Guzman Lovo <124549634+AndresGuzman04@users.noreply.github.com>
2024-09-16 18:11:05 +0200

    Practica 6

---
+++ practica 6/declaraciones.js
@@ -0,0 +1,13 @@
+var nombre = "Andres";
+console.log(nombre);
+// Output: Andres
+
+//Declaracion let
+
+let edad = 22;
+console.log(edad);
+
+//declaracion const
+const pais = "El Salvador"
+console.log(pais);
+
---
+++ practica 6/eventos.js
@@ -0,0 +1,5 @@
+let boton = document.getElementById("miBoton");
+boton.addEventListener("click", function(){
+    console.log("Has pulsado el botón");
+    alert("Se hizo click en el boton")
+});
